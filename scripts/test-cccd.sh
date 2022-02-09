bdae9061a3734ddbedf61f22c25388fa44a21e5b
Florian Sihler <florian.sihler@uni-ulm.de>
2022-02-09 21:35:09 +0100

    automatically identify name of cccd output

--- scripts/test-cccd.sh
+++ scripts/test-cccd.sh
@@ -9,11 +9,13 @@ cd "$CCCD_DIRTY/bashScripts"
 
 ./cccd "$TEST_NAME"
 
+OUTPUT_FILENAME=$(tail --lines=1 ../src/cccd-run.log)
+OUTPUT_FILENAME=${OUTPUT_FILENAME/See Report at: /}
+echo "analyze filename '$OUTPUT_FILENAME'"
+
 # cat "../sourceFiles/Reports/${TEST_NAME}_comparisionReport.csv" # jepp... a typo... great
 echo "validate test results..."
 python3 /validate-test.py /kraw-expected.csv "../sourceFiles/Reports/${TEST_NAME}_comparisionReport.csv"
-
-# TODO: automatically grep which file to cat for the test-cccd.sh script by using output of cccd (java call)
 
 # TODO: multiple source files => move source file extraction to highest level possible and remove test generation output from docker container build  after check
 # Do this by directory binding which allows for custom files
