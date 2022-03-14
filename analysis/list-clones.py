047135e53ecdca08785944874c1c5a97fca80743
Florian Sihler <florian.sihler@uni-ulm.de>
2022-03-14 02:29:29 +0100

    update clone detection list

--- analysis/list-clones.py
+++ analysis/list-clones.py
@@ -21,18 +21,18 @@ if __name__ == '__main__':
         print(f"usage: {sys.argv[0]} <mapping.csv>")
         exit(1)
     mapping = load_leven_dist_mapping(sys.argv[1])
-    t1 = print_clone_type(mapping, 1, list(range(0, 11)))
-    t2 = print_clone_type(mapping, 2, list(range(10, 21)))
-    t3 = print_clone_type(mapping, 3, list(range(20, 31)))
-    t4 = print_clone_type(mapping, 4, list(range(30, 41)))
+    t1 = print_clone_type(mapping, 1, list(range(0, 9)))
+    t2 = print_clone_type(mapping, 2, list(range(9, 18)))
+    t3 = print_clone_type(mapping, 3, list(range(18, 27)))
+    t4 = print_clone_type(mapping, 4, list(range(30, 36)))
 
     print("  - others")
     other = 0
     for diff, leven in mapping.items():
-        if leven > 40:
+        if leven > 35:
             other += 1
             print("    -", diff, f"[{leven}]")
 
     print("======= Clones ======= ")
     print(f"Type-1: {t1}\nType-2: {t2}\nType-3: {t3}\nType-4: {t4}")
-    print(f"Others: {other}")
+    print(f"Clones: {t1+t2+t3+t4} --- Others: {other}")
